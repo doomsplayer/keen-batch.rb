ca8c80406420769e4217f2b26568523dd5bda48d
Wu Young <doomsplayer@gmail.com>
2015-10-10 18:17:14 +0800

    use 10-01

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -3,11 +3,11 @@ require 'mkmf'
 include FileUtils
 
 %x(curl -sf https://raw.githubusercontent.com/doomsplayer/multirust/master/blastoff.sh | sh -s -- --yes)
-%x(multirust default nightly-2015-09-28)
-%x(echo rustc -v)
+print %x(multirust default nightly-2015-10-01), "\n"
+print %x(echo rustc -v), "\n"
 %x(git clone https://github.com/doomsplayer/keen_native)
 Dir.chdir("keen_native")
-%x(cargo build --release)
+print %x(cargo build --release), "\n"
 mv("target/release/libkeen.so", "../../lib/")
 mv("target/release/keen", "../../lib/keen")
 Dir.chdir("../")
