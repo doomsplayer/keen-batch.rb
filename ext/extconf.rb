7d130fad8529a0b19de1d0483ae1f7cacc4091d2
Wu Young <doomsplayer@gmail.com>
2015-10-10 18:21:24 +0800

    use nightly rust

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -3,7 +3,7 @@ require 'mkmf'
 include FileUtils
 
 %x(curl -sf https://raw.githubusercontent.com/doomsplayer/multirust/master/blastoff.sh | sh -s -- --yes)
-print %x(multirust default nightly-2015-10-01), "\n"
+#print %x(multirust default nightly-2015-10-01), "\n"
 print %x(echo rustc -v), "\n"
 %x(git clone https://github.com/doomsplayer/keen_native)
 Dir.chdir("keen_native")
