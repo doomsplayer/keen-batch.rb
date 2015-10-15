835f44756a463c0110b053779695e002ace97a42
Wu Young <doomsplayer@gmail.com>
2015-10-15 22:52:55 +0800

    use submodule to store native code

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -5,7 +5,6 @@ include FileUtils
 %x(curl -sf https://raw.githubusercontent.com/doomsplayer/multirust/master/blastoff.sh | sh -s -- --yes)
 #print %x(multirust default nightly-2015-10-01), "\n"
 print %x(echo rustc -v), "\n"
-%x(git clone https://github.com/doomsplayer/keen_native)
 Dir.chdir("keen_native")
 print %x(cargo build --release), "\n"
 mv("target/release/libkeen_native.so", "../../lib/")
