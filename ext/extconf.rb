5740e4b44361e5f2e60631f1064b0d9871f37a44
Young Woo <doomsplayer@gmail.com>
2016-01-04 17:24:30 +0800

    Update extconf.rb

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,10 +1,6 @@
 require 'fileutils'
 require 'mkmf'
 include FileUtils
-
-%x(curl -sf https://static.rust-lang.org/rustup.sh | sudo sh -s -- --yes)
-Dir.chdir(File.expand_path("../keen_native", __FILE__))
-print %x(cargo build --release), "\n"
-mv("target/release/libkeenio_booster.so", "../../lib/")
-Dir.chdir("../")
+%x(cargo build --release --manifest-path ./keen_native/Cargo.toml)
+mv("./keen_native/target/release/libkeenio_booster.so", "../lib/")
 create_makefile(".")
