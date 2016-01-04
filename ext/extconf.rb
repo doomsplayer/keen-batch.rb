a5038efea76f020b2a06feaa893948ba6eefd2e5
Young Woo <doomsplayer@gmail.com>
2016-01-04 17:31:38 +0800

    Update extconf.rb

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,6 +1,33 @@
 require 'fileutils'
 require 'mkmf'
+require 'rb_config'
 include FileUtils
+def os
+  host_os = RbConfig::CONFIG['host_os']
+  case host_os
+    when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+      :windows
+    when /darwin|mac os/
+      :macosx
+    when /linux/
+      :linux
+    when /solaris|bsd/
+      :unix
+    else
+      raise Error::WebDriverError, "unknown os: #{host_os.inspect}"
+    end
+end
+
 %x(cargo build --release --manifest-path ./keen_native/Cargo.toml)
-mv("./keen_native/target/release/libkeenio_booster.so", "../lib/")
+case os
+  when :linux
+    mv("./keen_native/target/release/libkeenio_booster.so", "../lib/")
+  when :unix
+    mv("./keen_native/target/release/libkeenio_booster.so", "../lib/")
+  when :macosx
+    mv("./keen_native/target/release/libkeenio_booster.dylib", "../lib/")
+  when :windows
+    mv("./keen_native/target/release/libkeenio_booster.dll", "../lib/")
+  end
+
 create_makefile(".")
